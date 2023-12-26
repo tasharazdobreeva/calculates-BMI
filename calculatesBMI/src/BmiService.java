57f5270a589e144747ecf51c3dbacbc18efadf68
natali.r <natasha-razdobreeva@mail.ru>
2023-12-26 21:38:15 +0300

    calculates BMI 2

---
+++ calculatesBMI/src/BmiService.java
@@ -0,0 +1,13 @@
+public class BmiService {
+    public int calculate (double kg, double m) {
+ //       double x;
+        int x = (int) (kg/(m*m));
+ //       int x = (int) (kg*cm);
+ //       int x = (int) x
+ //
+        return x;
+    }
+
+}
+
+
